79afdee51765c3ad73120c8a83a8b24926ebeaba
benjamin <2000beni770@gmail.com>
2025-08-04 15:46:23 +0300

    add drop_un_classified_rows func

--- src/clean_data.py
+++ src/clean_data.py
@@ -11,9 +11,13 @@ class Cleaner:
         lower_text = text_col.str.lower()
         return lower_text
 
+    def drop_un_classified_rows(self):
+        mask = (self.df['Biased'] == 0) | (self.df['Biased'] == 1)
+        self.df = self.df[mask]
+
     def clean(self):
-        text_col = self.df['Text']
-        clean_text = self.drop_punctuation_marks(text_col)
-        lower_text = self.convert_to_lowercase(clean_text)
-        self.df['Text'] = lower_text
-        # self.drop_un_classified_rows()
+        text_col = self.df['Text'] ## get the text col
+        clean_text = self.drop_punctuation_marks(text_col)#clean col from punctuation_marks
+        lower_text = self.convert_to_lowercase(clean_text)#convert it to lowercase
+        self.df['Text'] = lower_text ## set the new clean col as a df col
+        self.drop_un_classified_rows()
