19cfe155059c17cbaf9d218bd043b8fc380ee1f9
benjamin <2000beni770@gmail.com>
2025-08-04 15:47:43 +0300

    add error handling to load data

--- src/load_data.py
+++ src/load_data.py
@@ -2,5 +2,8 @@ import pandas as pd
 class Loader:
     @staticmethod
     def load_data(path):
-        df = pd.read_csv(path)
+        try:
+            df = pd.read_csv(path)
+        except Exception as e:
+            raise RuntimeError("error when trying to load csv") from e
         return df
