0b3167c205926227abb31cc7dd1851a8332ac215
benjamin <2000beni770@gmail.com>
2025-08-04 16:35:21 +0300

    split main to 2 func

--- src/main.py
+++ src/main.py
@@ -3,17 +3,22 @@ from parse_data import Parser
 from json_write import JsonWriter
 from clean_data import Cleaner
 
-def main():
-    data_path = "../data/tweets_dataset.csv"
-    df = IoCsv.load_data(data_path)
+def analyzation(df):
     parser = Parser(df)
     data_result = parser.parse()
     JsonWriter.write2json(data_result)
+
+def cleaning(df):
     cleaner = Cleaner(df)
     clean_df = cleaner.clean()
     data_result_path = "../results/tweets_dataset_cleaned.csv"
     IoCsv.write_data(data_result_path, clean_df)
 
+def main():
+    data_path = "../data/tweets_dataset.csv"
+    df = IoCsv.load_data(data_path)
+    analyzation(df)
+    cleaning(df)
 
-
-main()
+if __name__ == "__main__":
+    main()
