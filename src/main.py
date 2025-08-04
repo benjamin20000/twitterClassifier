a1d4ff00a7b45100242e384600676eed5ab5c9f7
benjamin <2000beni770@gmail.com>
2025-08-04 16:12:56 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from load_data import Loader
+from io_csv import IoCsv
 from parse_data import Parser
-
+from json_write import JsonWriter
+from clean_data import Cleaner
 
 def main():
     data_path = "../data/tweets_dataset.csv"
-    df = Loader.load_data(data_path)
+    df = IoCsv.load_data(data_path)
     parser = Parser(df)
-    parser.parse()
-    # print(df)
+    data_result = parser.parse()
+    JsonWriter.write2json(data_result)
+    cleaner = Cleaner(df)
+    clean_df = cleaner.clean()
+    data_result_path = "../results/tweets_dataset_cleaned.csv"
+    IoCsv.write_data(data_result_path, clean_df)
+
 
 
 main()
