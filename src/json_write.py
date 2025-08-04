dc46d8b0f1db1266d48463226feb951efa87e962
benjamin <2000beni770@gmail.com>
2025-08-04 15:17:55 +0300

    add json writer class

---
+++ src/json_write.py
@@ -0,0 +1,31 @@
+import json
+import numpy as np
+
+class JsonWriter:
+    @staticmethod
+    def write2json(dic):
+        try:
+            dic = JsonWriter.convert_to_native(dic)
+            with open('../results/json.results', 'w', encoding='utf-8') as f:
+                json.dump(dic, f, ensure_ascii=False, indent=4)
+        except Exception as e:
+            raise RuntimeError("error when trying to write to json") from e
+
+    """this function converting a numpy object
+     to native python obj suitable to json"""
+    @staticmethod
+    def convert_to_native(obj):
+        if isinstance(obj, dict):
+            return {JsonWriter.convert_to_native(k): JsonWriter.convert_to_native(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [JsonWriter.convert_to_native(i) for i in obj]
+        elif isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.bool_):
+            return bool(obj)
+        else:
+            return obj
+
+
