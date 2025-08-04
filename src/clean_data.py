1d26f47177a8436d46f59d23aea3932e9a8bbd90
benjamin <2000beni770@gmail.com>
2025-08-04 15:41:41 +0300

    add convert to lowercase func

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,9 +1,19 @@
 class Cleaner:
-    def drop_punctuation_marks(self, df):
+    def __init__(self, df):
+        self.df = df
+
+    def drop_punctuation_marks(self,text_col):
         symbols = "!#$%&()*+-./:,;<=>?[]^_`{|}~\n"
-        dataClean = df['Text']
-        dataClean = dataClean.str.translate({ord(symbol): "" for symbol in symbols})
-        print(dataClean)
-        # print(df['Text'])
-    def clean(self, df):
-        self.drop_punctuation_marks(df)
+        clean_text = text_col.str.translate({ord(symbol): "" for symbol in symbols})
+        return clean_text
+
+    def convert_to_lowercase(self, text_col):
+        lower_text = text_col.str.lower()
+        return lower_text
+
+    def clean(self):
+        text_col = self.df['Text']
+        clean_text = self.drop_punctuation_marks(text_col)
+        lower_text = self.convert_to_lowercase(clean_text)
+        self.df['Text'] = lower_text
+        # self.drop_un_classified_rows()
