42f4d0ad0cf11ee9ba583b04b2795ad7dbb79e42
benjamin <2000beni770@gmail.com>
2025-08-04 10:13:05 +0300

    add count_classes func

--- src/parse_data.py
+++ src/parse_data.py
@@ -4,5 +4,10 @@ class Parser:
         self.df = df
 
     def count_classes(self):
-        print(self.df.value_counts("Biased"))
+        counts = self.df.value_counts("Biased")
+        result = {"antisemitic": counts[1],
+                    "non_antisemitic":counts[0],
+                    "total": counts[0] + counts[1]} ## TODO add unspecified
+        return result
+
 
